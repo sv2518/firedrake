1dd2c3d2927b37d92faf52907cf7481a3a5d929d
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-05-29 20:50:30 +0200

    Fix coeff map

--- firedrake/slate/slac/compiler.py
+++ firedrake/slate/slac/compiler.py
@@ -209,7 +209,8 @@ def generate_loopy_kernel(slate_expr, compiler_parameters=None, diagonal=False):
 
     new_coeffs = slate_expr.coefficients()
     orig_coeffs = orig_expr.coefficients()
-    get_index = lambda n: orig_coeffs.index(new_coeffs[n]) if new_coeffs[n] in orig_coeffs else n
+    get_index = lambda n: orig_coeffs.index(new_coeffs[n]) if (not isinstance(new_coeffs[n], slate.BlockFunction) and new_coeffs[n] in orig_coeffs) else orig_coeffs.index(new_coeffs[n].orig_function) if (isinstance(new_coeffs[n], slate.BlockFunction) and new_coeffs[n].orig_function in orig_coeffs) else n
+    # get_index = lambda n: orig_coeffs.index(new_coeffs[n]) if new_coeffs[n] in orig_coeffs else n
     coeff_map =  (tuple((get_index(n), split_map) for (n, split_map) in slate_expr.coeff_map))
     coefficients = list(filter(lambda elm: isinstance(elm, CoefficientKernelArg), arguments))
     assert len(list(chain(*(map[1] for map in coeff_map)))) == len(coefficients), "KernelInfo must be generated with a coefficient map that maps EXACTLY all cofficients there are in its arguments attribute."
